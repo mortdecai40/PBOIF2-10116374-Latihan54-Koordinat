c9d4f02c67ecd0fb687b76ebae053e4a00daf6c5
mortdecai40 <rizkinabil.999@gmail.com>
2020-11-15 13:04:07 +0700

    Latihan 54

---
+++ src/pboif2/pkg10116374/latihan54/koordinat/PBOIF210116374Latihan54Koordinat.java
@@ -0,0 +1,25 @@
+package pboif2.pkg10116374.latihan54.koordinat;
+
+/**
+ * @author Acromyrmex
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk menghitung koordinat
+ *
+ */
+public class PBOIF210116374Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKoordinat warnaKoordinat = new WarnaKoordinat(10, 4, "Jingga");
+        
+        System.out.println("Warna Koordinat : " + warnaKoordinat.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : "
+                + warnaKoordinat.getX() + ", y : " + warnaKoordinat.getY());
+    }
+    
+}
---
+++ src/pboif2/pkg10116374/latihan54/koordinat/WarnaKoordinat.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10116374.latihan54.koordinat;
+
+/**
+ *
+ * @author Acromyrmex
+ */
+class WarnaKoordinat extends Koordinat {
+    public String namaWarna;
+    
+    public WarnaKoordinat(int x, int y, String namaWarna) {
+        super(x,y);
+        this.x = x;
+        this.y = y;
+        this.namaWarna = namaWarna;
+    }
+
+    public String getNamaWarna() {
+        return namaWarna;
+    }
+
+    public void setNamaWarna(String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+}
